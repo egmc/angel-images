80c030eaefe4e2302b58886bc9f5bf482c575232
egmc <waltz4debby@eg2mix.com>
2012-12-15 17:03:16 +0900

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 angel-images
 ============
 
-download images from ANGEL EYES
+image downloader for ANGEL EYES photo report
+
+## require
+
+composerとphp5.3以上
+
+## usage
+
+```
+git clone git://github.com/egmc/angel-images.git
+composer install
+php angel-images.php
+```
+
+## その他
+
+Goutteでログインが必要なページでアレコレするテスト。
+無茶しないでね。
---
+++ angel_images.php
@@ -0,0 +1,59 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+use Goutte\Client;
+
+$fc_url_prefix = 'http://fc.momoclo.net';
+// login
+$login_url = 'https://fc.momoclo.net/pc/login.php';
+$photo_url = $fc_url_prefix . '/pc/photo/';
+
+if ($argc < 2) {
+	die("usage: php angel_images.php your_id your_password [full path to save image(default create images dir here)]\n");
+}
+
+$login_id = $argv[1];
+$password = $argv[2];
+$target_dir =  __DIR__ . "/images";
+if (isset($argv[3])) {
+	$target_dir = $argv[3];
+} else {
+	mkdir($target_dir);
+	if(!is_dir($target_dir)) {
+		die("can't create images dir here");
+	}
+}
+
+$client = new Client();
+
+$crawler = $client->request('GET', $login_url);
+
+$form = $crawler->selectButton('logInButton')->form();
+		
+$crawler = $client->submit($form, array('login_id' => $login_id, 'password' => $password));
+
+$crawler = $client->request('GET', $photo_url);
+
+// get report url list
+$report_entry_urls = array_unique($crawler->filter('ul.reportList li a')->extract('href'));
+if(!$report_entry_urls) {
+	die("can't get report urls maybe login failed");
+}
+
+foreach ($report_entry_urls as $report_entry_url) {
+	$crawler = $client->request('GET',  $fc_url_prefix . $report_entry_url);
+	$image_urls = $crawler->filter('ul#photoList li a')->extract('href');
+	foreach ($image_urls as $image_url) {
+		$crawler = $client->request('GET',  $fc_url_prefix . $image_url);
+		$response = $client->getResponse();
+		//var_dump($content->getContent());
+		//break;
+		$filename = pathinfo($image_url, PATHINFO_BASENAME);
+		$save_path = $target_dir . "/" . $filename;
+		if(file_exists($save_path)) {
+			echo "$save_path::already exists\n";
+		} else {
+			file_put_contents($save_path, $response->getContent());
+			echo "$save_path::saved\n";
+		}
+	}
+}
